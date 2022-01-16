ce0f136eccda642765f5d409cc466bc0118cb87b
azuolas <azuolasbalbieris@gmail.com>
2022-01-17 00:09:03 +0200

    sql commands

--- app/src/main/java/com/sunnyoaklabs/manodienynas/domain/model/Event.kt
+++ app/src/main/java/com/sunnyoaklabs/manodienynas/domain/model/Event.kt
@@ -4,7 +4,7 @@ data class Event(
     val title: String,
     val pupilInfo: String,
     val createDate: String,
-    val createDateText: String?,
+    val createDateText: String,
     val eventHeader: String,
     val eventText: String,
     val creator_name: String,
